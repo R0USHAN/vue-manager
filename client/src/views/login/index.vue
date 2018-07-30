e9d6325c069db52a974545967b5642a19ea75a73
q8164305 <343164990@qq.com>
2018-07-30 20:15:36 +0800

    新增登录页面

---
+++ client/src/views/login/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                key: value
+            }
+        },
+        computed: {
+            name() {
+                return this.data 
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
+
+
